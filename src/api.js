96c0bd2038b1e4e74f3360acafee14cfa01619b5
maomao0007 <skylifepic07@gmail.com>
2025-02-09 21:53:17 +0800

    fix: update baseURL configuration for development and production environments

--- src/api.js
+++ src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "http://localhost:5001"
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://goshopping-backend.onrender.com"
+      : "http://localhost:5001",
 });
 
 export default api;
